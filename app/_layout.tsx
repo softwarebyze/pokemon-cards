629633c94244a9e8bc1ed9e6279cdfff02972505
Zachary Ebenfeld <zackebenfeld@gmail.com>
2025-03-12 12:40:35 -0400

    added gesture handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,6 +12,7 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/components/useColorScheme";
 import { ReactQueryProvider } from "@/ReactQueryProvider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -54,13 +55,17 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ReactQueryProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-        </Stack>
-      </ThemeProvider>
-    </ReactQueryProvider>
+    <GestureHandlerRootView>
+      <ReactQueryProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+          </Stack>
+        </ThemeProvider>
+      </ReactQueryProvider>
+    </GestureHandlerRootView>
   );
 }
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -2,17 +2,12 @@ import { StyleSheet, View } from "react-native";
 import { Text } from "./Themed";
 import { Image } from "expo-image";
 
-export const PokemonCard = ({
-  pokemon,
-}: {
-  pokemon: { name: string; exp: number; imgUrl: string };
-}) => {
+export type PokemonForCard = { name: string; exp: number; imgUrl: string };
+
+export const PokemonCard = ({ pokemon }: { pokemon: PokemonForCard }) => {
   return (
     <View style={styles.pokemonCard}>
-      <Image
-        source={{ uri: pokemon.imgUrl }}
-        style={{ width: 300, height: 300 }}
-      />
+      <Image style={styles.image} source={{ uri: pokemon.imgUrl }} />
       <Text style={styles.name}>{pokemon.name}</Text>
       <Text style={styles.exp}>{pokemon.exp}</Text>
     </View>
@@ -26,11 +21,16 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     borderRadius: 8,
     margin: "auto",
+    backgroundColor: "white",
   },
   name: {
     fontSize: 48,
   },
   exp: {
     fontSize: 20,
+  },
+  image: {
+    width: 300,
+    height: 300,
   },
 });
